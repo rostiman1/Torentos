7de94e939fdd642790415b0d3a2a8f8791e3f79a
Rostislav12 <unimaker@gmail.com>
2022-09-05 11:00:00 +0300

    Add project files.

---
+++ Torentos/Server/Data/DataContext.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Torentos.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 1,
+                Title = "League Of Legends",
+                Description = "What is League of Legends?League of Legends is a team-based strategy game where two teams of five powerful champions face off to destroy the other's base. Choose from over 140 champions to make epic plays, secure kills, and take down towers as you battle your way to victory.",
+                Image = "images/LeagueOfLegends.png",
+                Price = 9.99m,
+                Type = "images/VideoGame.png"
+            },
+             new Product
+             {
+                 Id = 2,
+                 Title = "Cars 2",
+                 Description = "Cars 2 is an animated movie that explains the life of all cars.",
+                 Image = "images/Cars2.png",
+                 Price = 5.40m,
+                 Type = "images/Movie.png"
+             },
+             new Product
+             {
+                 Id = 3,
+                 Title = "AC-DC",
+                 Description = "Back in Black is the seventh studio album by Australian rock band AC/DC. It was released on 25 July 1980 by Albert Productions and Atlantic Records. It is the band's first album to feature lead singer Brian Johnson, following the death of previous lead singer Bon Scott..",
+                 Image = "images/ACDC.png",
+                 Price = 2.30m,
+                 Type = "images/Music.png"
+             }
+         );
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Torentos/Shared/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torentos.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateUpdated { get; set; }
+        public string? Type { get; set; }
+    }
+}
